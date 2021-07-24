589590e82da50046723aeb0ab9564809e5b7f1d1
Shilpa <shilpamjoy@gmail.com>
2021-07-24 13:00:56 -0400

    Initial Commit

---
+++ CoreData_Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_Demo
+//
+//  Created by Shilpa Joy on 2021-07-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
